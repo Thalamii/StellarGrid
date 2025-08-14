ed57b2ed31b1db88fa10899377a681e495dc5e84
tabsubject <tabsubject@gmail.com>
2025-08-14 12:25:09 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://wordgrid.game",
+      url: "https://wordgrid.fun",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
